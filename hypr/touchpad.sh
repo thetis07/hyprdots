5b7dd38142a84b938eb8974de1f8106597a2955a
kerem <bftrade07@gmail.com>
2025-03-21 22:21:48 +0300

    Add files via upload
    Main release

---
+++ hypr/touchpad.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+HYPRLAND_DEVICE="elan06fa:00-04f3:327e-touchpad"
+HYPRLAND_VARIABLE="device[$HYPRLAND_DEVICE]:enabled"
+
+if [ -z "$XDG_RUNTIME_DIR" ]; then
+  export XDG_RUNTIME_DIR=/run/user/$(id -u)
+fi
+
+export STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
+
+enable_touchpad() {
+    printf "true" >"$STATUS_FILE"
+  notify-send -u normal "Enabling Touchpad"
+hyprctl keyword $HYPRLAND_VARIABLE "true" -r
+}
+
+disable_touchpad() {
+    printf "false" >"$STATUS_FILE"
+notify-send -u normal "Disabling Touchpad"
+hyprctl keyword $HYPRLAND_VARIABLE "false" -r
+}
+
+if ! [ -f "$STATUS_FILE" ]; then
+  enable_touchpad
+else
+  if [ $(cat "$STATUS_FILE") = "true" ]; then
+    disable_touchpad
+  elif [ $(cat "$STATUS_FILE") = "false" ]; then
+    enable_touchpad
+  fi
+fi
---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,11 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<leader>tt", ":lua require('base46').toggle_transparency()<CR>", { noremap = true, silent = true, desc = "Toggle Background Transparency" })
